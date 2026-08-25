7b4727b1fd5fd1598fdd183a37ee2f8559440a83
AlessoDev619 <ti.admin@alesso.com.mx>
2026-08-25 12:04:59 -0600

    Fix URL syntax error in index.js

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ app.get('/contabilidad', async (req, res) => {
     }
     */
 
-    const url = `https://${process.env.ACCOUNT_ID}.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=1255&deploy=1&typeQuery=contabilidad&startDate=${startDate}&tranType=${tranType]`;
+    const url = `https://${process.env.ACCOUNT_ID}.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=1255&deploy=1&typeQuery=contabilidad&startDate=${startDate}&tranType=${tranType}`;
 
     const request_data = {
         url,
